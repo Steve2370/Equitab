fe29196c542ccf16872bcc9a8154c9840ca28a70
Steve2370 <pougouebrice@gmail.com>
2026-07-04 23:32:57 -0400

    fix: symfony response type + remove Laravel from title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'Equitab') }}</title>
         <script src="https://js.stripe.com/v3/" defer></script>
 
         <!-- Fonts -->
